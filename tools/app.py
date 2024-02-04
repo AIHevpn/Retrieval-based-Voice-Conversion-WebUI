e3305080d336d015f0072444cc403c5c339a6c03
Rico Ardiansyah <131390720+laynz28@users.noreply.github.com>
2024-02-04 11:42:10 +0700

    Update app.py

--- tools/app.py
+++ tools/app.py
@@ -1,43 +1,159 @@
+import os, sys
+import datetime, subprocess
+from mega import Mega
+now_dir = os.getcwd()
+sys.path.append(now_dir)
 import logging
-import os
+import shutil
+import threading
+import traceback
+import warnings
+from random import shuffle
+from subprocess import Popen
+from time import sleep
+import json
+import pathlib
 
-# os.system("wget -P cvec/ https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/hubert_base.pt")
+import fairseq
+import faiss
 import gradio as gr
+import numpy as np
+import torch
 from dotenv import load_dotenv
+from sklearn.cluster import MiniBatchKMeans
 
 from configs.config import Config
 from i18n.i18n import I18nAuto
+from infer.lib.train.process_ckpt import (
+    change_info,
+    extract_small_model,
+    merge,
+    show_info,
+)
+from infer.modules.uvr5.modules import uvr
 from infer.modules.vc.modules import VC
-
 logging.getLogger("numba").setLevel(logging.WARNING)
-logging.getLogger("markdown_it").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-logging.getLogger("matplotlib").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
 
-i18n = I18nAuto()
-logger.info(i18n)
+tmp = os.path.join(now_dir, "TEMP")
+shutil.rmtree(tmp, ignore_errors=True)
+shutil.rmtree("%s/runtime/Lib/site-packages/infer_pack" % (now_dir), ignore_errors=True)
+shutil.rmtree("%s/runtime/Lib/site-packages/uvr5_pack" % (now_dir), ignore_errors=True)
+os.makedirs(tmp, exist_ok=True)
+os.makedirs(os.path.join(now_dir, "logs"), exist_ok=True)
+os.makedirs(os.path.join(now_dir, "assets/weights"), exist_ok=True)
+os.environ["TEMP"] = tmp
+warnings.filterwarnings("ignore")
+torch.manual_seed(114514)
+
 
 load_dotenv()
 config = Config()
 vc = VC(config)
 
-weight_root = os.getenv("weight_root")
-weight_uvr5_root = os.getenv("weight_uvr5_root")
-index_root = os.getenv("index_root")
-names = []
-hubert_model = None
-for name in os.listdir(weight_root):
-    if name.endswith(".pth"):
-        names.append(name)
-index_paths = []
-for root, dirs, files in os.walk(index_root, topdown=False):
-    for name in files:
-        if name.endswith(".index") and "trained" not in name:
-            index_paths.append("%s/%s" % (root, name))
+
+def download_from_url(url, model):
+    if model =='':
+        try:
+            model = url.split('/')[-1]
+        except:
+            return "You need to name your model. For example: My-Model"
+    url=url.replace('/blob/main/','/resolve/main/')
+    model=model.replace('.pth','').replace('.index','').replace('.zip','')
+    if url == '':
+        return "URL cannot be left empty."
+    url = url.strip()
+    zip_dirs = ["zips", "unzips"]
+    for directory in zip_dirs:
+        if os.path.exists(directory):
+            shutil.rmtree(directory)
+    os.makedirs("zips", exist_ok=True)
+    os.makedirs("unzips", exist_ok=True)
+    zipfile = model + '.zip'
+    zipfile_path = './zips/' + zipfile
+    try:
+        if url.endswith('.pth'):
+            subprocess.run(["wget", url, "-O", f'./assets/weights/{model}.pth'])
+            return f"Sucessfully downloaded as {model}.pth"
+        if url.endswith('.index'):
+            if not os.path.exists(f'./logs/{model}'): os.makedirs(f'./logs/{model}')
+            subprocess.run(["wget", url, "-O", f'./logs/{model}/added_{model}.index'])
+            return f"Successfully downloaded as added_{model}.index"
+        if "drive.google.com" in url:
+            subprocess.run(["gdown", url, "--fuzzy", "-O", zipfile_path])
+        elif "mega.nz" in url:
+            m = Mega()
+            m.download_url(url, './zips')
+        else:
+            subprocess.run(["wget", url, "-O", zipfile_path])
+        for filename in os.listdir("./zips"):
+            if filename.endswith(".zip"):
+                zipfile_path = os.path.join("./zips/",filename)
+                shutil.unpack_archive(zipfile_path, "./unzips", 'zip')
+            else:
+                return "No zipfile found."
+        for root, dirs, files in os.walk('./unzips'):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if file.endswith(".index"):
+                    os.mkdir(f'./logs/{model}')
+                    shutil.copy2(file_path,f'./logs/{model}')
+                elif "G_" not in file and "D_" not in file and file.endswith(".pth"):
+                    shutil.copy(file_path,f'./assets/weights/{model}.pth')
+        shutil.rmtree("zips")
+        shutil.rmtree("unzips")
+        return "Success."
+    except:
+        return "There's been an error."
+
+def upload_to_dataset(files, dir):
+    if dir == '':
+        dir = './dataset/'+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    for file in files:
+        path=file.name
+        shutil.copy2(path,dir)
+    try:
+        gr.Info(i18n("处理数据"))
+    except:
+        pass
+    return i18n("处理数据"), {"value":dir,"__type__":"update"}
+
+def download_model_files(model):
+    model_found = False
+    index_found = False
+    if os.path.exists(f'./assets/weights/{model}.pth'): model_found = True
+    if os.path.exists(f'./logs/{model}'):
+        for file in os.listdir(f'./logs/{model}'):
+            if file.endswith('.index') and 'added' in file:
+                log_file = file
+                index_found = True
+    if model_found and index_found:
+        return [f'./assets/weights/{model}.pth', f'./logs/{model}/{log_file}'], "Done"
+    elif model_found and not index_found:
+        return f'./assets/weights/{model}.pth', "Could not find Index file."
+    elif index_found and not model_found:
+        return f'./logs/{model}/{log_file}', f'Make sure the Voice Name is correct. I could not find {model}.pth'
+    else:
+        return None, f'Could not find {model}.pth or corresponding Index file.'
+
+def update_visibility(visible):
+    if visible:
+        return {"visible":True,"__type__":"update"},{"visible":True,"__type__":"update"}
+    else:
+        return {"visible":False,"__type__":"update"},{"visible":False,"__type__":"update"}
+
+def get_pretrains(string):
+    pretrains = []
+    for file in os.listdir('assets/pretrained_v2'):
+        if string in file:
+            pretrains.append(os.path.join('assets/pretrained_v2',file))
+    return pretrains
 
 
-app = gr.Blocks()
+with gr.Blocks(title="🔊 RVC",theme=gr.themes.Soft(primary_hue="purple",neutral_hue="zinc")) as app:
 with app:
     with gr.Tabs():
         with gr.TabItem("RVC EASY INFERENCE"):
@@ -138,6 +254,17 @@ with app:
                 ],
                 [vc_output1, vc_output2],
             )
-
+   with gr.TabItem("Download Model"):
+            with gr.Row():
+                url=gr.Textbox(label="Enter the URL to the Model:")
+            with gr.Row():
+                model = gr.Textbox(label="Name your model:")
+                download_button=gr.Button("Download")
+            with gr.Row():
+                status_bar=gr.Textbox(label="")
+                download_button.click(fn=download_from_url, inputs=[url, model], outputs=[status_bar])
+            with gr.Row():
+                gr.Markdown(
+            )
 
 app.launch(share=True)
